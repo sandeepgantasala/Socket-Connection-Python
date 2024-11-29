6440eb4b606716620cba052f004641e6f780c840
sandeep <sandeepgantasala@gmail.com>
2024-11-29 10:47:22 -0700

    Added some changes

--- clientsocket.py
+++ clientsocket.py
@@ -1,25 +1,13 @@
-# import socket
-# c=socket.socket()
-
-# print("Socket Created")
-
-# c.connect(("localhost",9999))
-# name=input("Enter your name")
-# c.send(bytes(name,"utf-8"))
-
-
-# print(c.recv(1024).decode())
-
 
 import socket
 
-client=socket.socket()
+client=socket.socket()                 #creating object socket connection
 
 print("Socket created for client")
 
-client.connect(("192.168.0.165",1234))
+client.connect(("192.168.0.165",1234))   #should connect to server side ip address
 
 print("connected to server")
 name=input("Enter your Name here ")
-client.send(bytes(name,"utf-8"))
-print(client.recv(1024).decode())
+client.send(bytes(name,"utf-8"))           #sending the message as name 
+print(client.recv(1024).decode())          #recieving response from server as message
